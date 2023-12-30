a2c0399dacd02366c7d42d9aec35f62527a165b3
nandakumar-kaliappan <nandakumarkalliappan@gmail.com>
2023-12-30 21:31:55 +0530

    created Customer entity. closes #3

---
+++ src/main/java/com/example/billingsoft/domain/BaseEntity.java
@@ -0,0 +1,30 @@
+package com.example.billingsoft.domain;
+
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@MappedSuperclass
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+public class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private Timestamp createdDate;
+
+    @UpdateTimestamp
+    @Basic
+    private Timestamp lastModifiedDate;
+
+
+}
---
+++ src/main/java/com/example/billingsoft/domain/Customer.java
@@ -0,0 +1,18 @@
+package com.example.billingsoft.domain;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Customer extends  BaseEntity{
+    private String name;
+    private String address;
+    private String phone;
+}
