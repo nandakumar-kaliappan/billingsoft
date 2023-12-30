02d439aad40dc31d85abb558b4505ad2740323ad
nandakumar-kaliappan <nandakumarkalliappan@gmail.com>
2023-12-30 18:44:34 +0530

    Initial commit

---
+++ src/main/java/com/example/billingsoft/BillingsoftApplication.java
@@ -0,0 +1,13 @@
+package com.example.billingsoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillingsoftApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BillingsoftApplication.class, args);
+    }
+
+}
